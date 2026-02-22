8012e32790d34c7db74409286ab1e58f65543dec
Mahendran ramar <mahendran.ramar@konnectify.co>
2026-02-22 18:18:23 +0530

    updates

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -397,7 +397,7 @@ user_router.post("/generateOTP", async (req, res) => {
     req.app.locals.OTP = OTP;
 
     // Send email
-    await sendMail({
+    sendMail({
       to: email,
       subject: "Your OTP Code",
       username: email,
